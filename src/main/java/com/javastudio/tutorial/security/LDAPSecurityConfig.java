fa6eff89612ac2fd01b85ac1b7503f97ef38b980
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2020-12-25 16:13:57 +0330

    Spring Boot Security Ldap

--- src/main/java/com/javastudio/tutorial/security/LDAPSecurityConfig.java
+++ src/main/java/com/javastudio/tutorial/security/LDAPSecurityConfig.java
@@ -6,15 +6,20 @@
 package com.javastudio.tutorial.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 
 /**
  * @author addico
  */
 @Configuration
+@EnableWebSecurity
 public class LDAPSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${ldap.urls}")
@@ -55,14 +60,23 @@ public class LDAPSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .ldapAuthentication()
-                .contextSource()
-                .url(ldapUrls + ldapBaseDn)
-                .managerDn(ldapSecurityPrincipal)
-                .managerPassword(ldapPrincipalPassword)
-                .and()
-                .userDnPatterns(ldapUserDnPattern);
+//        auth
+//                .ldapAuthentication()
+//                .contextSource()
+//                .url(ldapUrls + ldapBaseDn)
+//                .managerDn(ldapSecurityPrincipal)
+//                .managerPassword(ldapPrincipalPassword)
+//                .and()
+//                .userDnPatterns(ldapUserDnPattern);
+
+        auth.authenticationProvider(activeDirectoryLdapAuthenticationProvider());
     }
 
+    @Bean
+    public AuthenticationProvider activeDirectoryLdapAuthenticationProvider() {
+        ActiveDirectoryLdapAuthenticationProvider provider = new ActiveDirectoryLdapAuthenticationProvider("adldap.company.com", "ldap://adldap.company.com");
+        provider.setConvertSubErrorCodesToExceptions(true);
+        provider.setUseAuthenticationRequestCredentials(true);
+        return provider;
+    }
 }
